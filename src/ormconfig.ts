22cc032bb36aae84040ff7d3c33263ac10407a43
Vasylenko Paul <maincrafter1307@gmail.com>
2023-07-12 21:03:51 +0300

    Add database connection via typeorm module

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumuser',
+  password: '123',
+  database: 'medium_db',
+};
+
+export default config;
